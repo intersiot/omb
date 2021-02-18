cfaf82eecde9dff0c981fe7b2749a91bc39c79bc
intersiot <maybeormaybenot.030@gmail.com>
2021-02-18 16:07:48 +0900

    프래그먼트 레이아웃 디자인 하고 있음

--- app/src/main/java/com/intersiot/ohmybank/fragment/AllMenuFragment.kt
+++ app/src/main/java/com/intersiot/ohmybank/fragment/AllMenuFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.intersiot.ohmybank.R
+import kotlin.random.Random
 
 class AllMenuFragment : Fragment() {
 
@@ -18,6 +19,11 @@ class AllMenuFragment : Fragment() {
     // 계좌 이체 버튼
     fun accountTransfer(view: View) {
         
+    }
+
+    // 계좌 생성
+    fun btnAccountCreate(view: View) {
+        // 랜덤함수 사용해서 계좌 만들기 (중복제거)
     }
 
     // 로그아웃 버튼
