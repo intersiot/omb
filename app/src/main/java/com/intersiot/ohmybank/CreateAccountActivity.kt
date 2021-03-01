0d02650ee0ed791fe591999ee115639d226afeb3
intersiot <maybeormaybenot.030@gmail.com>
2021-03-01 21:58:52 +0900

    계좌생성 랜덤함수 이용해서

--- app/src/main/java/com/intersiot/ohmybank/CreateAccountActivity.kt
+++ app/src/main/java/com/intersiot/ohmybank/CreateAccountActivity.kt
@@ -3,16 +3,21 @@ package com.intersiot.ohmybank
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import com.intersiot.ohmybank.databinding.ActivityCreateAccountBinding
+import com.intersiot.ohmybank.databinding.ActivityMainBinding
 import java.util.*
 
 class CreateAccountActivity : AppCompatActivity() {
     private val tag: String = "CreateAccountActivity"
-
-
+    // layout view
+    private lateinit var binding: ActivityCreateAccountBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_account)
+        binding = ActivityCreateAccountBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
     /**
@@ -20,7 +25,7 @@ class CreateAccountActivity : AppCompatActivity() {
      * 1. 중복이 되지 않도록 처리해야함.
      * 2. db에 저장되어야 함 -> firebase
      */
-    fun onCreateAccount() {
+    fun onCreateAccount(view: View) {
         Log.d(tag, "계좌생성 선택됨")
     }
 
